012266caa0cb88382aec72912e21c56d6096e1a7
Samarth <samkusaxena@gmail.com>
2023-05-14 11:36:30 +0530

    added components file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "../styles/globals.css";
 
+import { Footer, Navbar } from "../Components";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
