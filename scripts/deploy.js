aebb725d6c39f6fe03fb4d5c10b12ef76c0be2ca
Samarth <samkusaxena@gmail.com>
2023-05-14 12:21:11 +0530

    doployed smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,20 @@
 const hre = require("hardhat");
 
+//0x5FbDB2315678afecb367f032d93F642f64180aa3
+
 async function main() {
-  const CrowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
-  const crowdFunding = await CrowdFunding.deploy(unlockTime, {
-    value: lockedAmount,
-  });
+  const crowdFunding = await hre.ethers.getContractFactory("CrowdFunding");
+  const CrowdFunding = await crowdFunding.deploy();
 
-  await crowdFunding.deployed();
-
-  console.log(`CrowdFunding deployed to ${crowdFunding.address}`);
+  await CrowdFunding.deployed();
+  console.log("CrowdFunding deployed to:", CrowdFunding.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
